d2fa7393e3a0573e7e76b590df3a5d8532943c62
Ted Leahy <leahyted@proton.me>
2025-03-10 23:53:19 +0000

    Use React state to track the location of the Knight, instead of using the observer function.
    This will allow for tracking other pieces more easily

--- src/components/BoardSquare.tsx
+++ src/components/BoardSquare.tsx
@@ -2,22 +2,36 @@ import { PropsWithChildren } from 'react';
 import Square from './Square';
 import { DropTargetMonitor, useDrop } from 'react-dnd';
 import { ItemTypes } from '../constants';
-import { moveKnight, validKnightMove } from '../utils';
+import { validKnightMove } from '../utils';
 import Overlay from './Overlay';
+import { PiecePositions } from './Board';
 
 interface BoardSquareProps {
   x: number;
   y: number;
+  setPiecePositions: React.Dispatch<React.SetStateAction<PiecePositions>>;
 }
 
-export default function BoardSquare({ x, y, children }: PropsWithChildren<BoardSquareProps>) {
+export default function BoardSquare({
+  x,
+  y,
+  setPiecePositions,
+  children,
+}: PropsWithChildren<BoardSquareProps>) {
   const color = (x + y) % 2 === 1 ? 'black' : 'white';
 
   const [{ isOver, canDrop }, drop] = useDrop(
     () => ({
       accept: ItemTypes.KNIGHT,
-      canDrop: () => validKnightMove(x, y),
-      drop: () => moveKnight(x, y),
+      canDrop: (item: { x: number; y: number }) => validKnightMove(x, y, item.x, item.y),
+      drop: (item: { x: number; y: number }) => {
+        setPiecePositions((prev: PiecePositions) => {
+          const newPiecePositions = { ...prev };
+          delete newPiecePositions[`${item.x},${item.y}`];
+          newPiecePositions[`${x},${y}`] = 'knight';
+          return newPiecePositions;
+        });
+      },
       collect: (monitor: DropTargetMonitor) => ({
         isOver: !!monitor.isOver(),
         canDrop: !!monitor.canDrop(),
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { observe } from './utils.ts';
 import Board from './components/Board.tsx';
 
-const root = createRoot(document.getElementById('root')!);
-
-observe((knightPosition: number[]) => {
-  root.render(
-    <StrictMode>
-      <Board boardSize={800} knightPosition={knightPosition} />
-    </StrictMode>,
-  );
-});
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Board />
+  </StrictMode>,
+);
