ba48af1ff349e8794d0489f521d8650c57bf2a8c
Ted Leahy <leahyted@proton.me>
2025-03-10 23:53:20 +0000

    Add rook piece

--- src/utils/moveValidations.ts
+++ src/utils/moveValidations.ts
@@ -4,6 +4,8 @@ export function validMove(from: number[], to: number[], piece: string) {
       return validKnightMove(from, to);
     case 'bishop':
       return validBishopMove(from, to);
+    case 'rook':
+      return validRookMove(from, to);
     default:
       return false;
   }
@@ -25,4 +27,14 @@ function validBishopMove([fromX, fromY]: number[], [toX, toY]: number[]) {
   // i.e. if it moves one square up/down, it must also move one square left/right.
   // It must also move at least one square.
   return dx === dy && dx > 0;
+}
+
+function validRookMove([fromX, fromY]: number[], [toX, toY]: number[]) {
+  const dx = Math.abs(toX - fromX);
+  const dy = Math.abs(toY - fromY);
+
+  // A rook moves horizontally or vertically, so can only move in one axis at a time.
+  // That means one axis must stay the same. It must also move at least one square
+  // along either axis.
+  return (dx === 0 || dy === 0) && (dx > 0 || dy > 0);
 }
