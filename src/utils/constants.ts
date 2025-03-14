afbc9f8c55995fab05e2c667c8da6397e32ae8f6
Ted Leahy <leahyted@proton.me>
2025-03-14 22:21:58 +0000

    Add king piece

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,7 @@ import knightImage from '../assets/drag-preview-images/knight.png';
 import bishopImage from '../assets/drag-preview-images/bishop.png';
 import rookImage from '../assets/drag-preview-images/rook.png';
 import queenImage from '../assets/drag-preview-images/queen.png';
+import kingImage from '../assets/drag-preview-images/king.png';
 
 export const ItemTypes = {
   PIECE: 'piece',
@@ -15,6 +16,7 @@ export const PIECE_CHARS: Record<string, string> = {
   bishop: '♗',
   rook: '♖',
   queen: '♕',
+  king: '♔',
 };
 
 // Map from coordinates to piece name
@@ -25,6 +27,7 @@ export const initialPiecePositions: PiecePositions = {
   '1,0': 'knight',
   '2,0': 'bishop',
   '3,0': 'queen',
+  '4,0': 'king',
   '5,0': 'bishop',
   '6,0': 'knight',
   '7,0': 'rook',
@@ -36,4 +39,5 @@ export const DRAG_PREVIEW_IMAGES: Record<string, string> = {
   bishop: bishopImage,
   rook: rookImage,
   queen: queenImage,
+  king: kingImage,
 };
