e622594279c5bbc0c7da30a0da6fdda791e114a5
Ted Leahy <leahyted@proton.me>
2025-03-14 22:22:01 +0000

    Prevent pieces from moving onto squares that already contain a piece of the same colour

--- src/utils/moveValidations.ts
+++ src/utils/moveValidations.ts
@@ -1,77 +1,136 @@
-import { PieceColor } from './constants';
+import { PieceDropItem } from '../components/BoardSquare';
+import { PieceColor, PiecePositions } from './constants';
 
-export function validMove(from: number[], to: number[], piece: string, color?: PieceColor) {
-  switch (piece) {
+export function validMove(
+  { x, y, name, color }: PieceDropItem,
+  [toX, toY]: number[],
+  piecePositions: PiecePositions,
+) {
+  const from = [x, y];
+  const to = [toX, toY];
+
+  switch (name) {
     case 'knight':
-      return validKnightMove(from, to);
+      return validKnightMove(from, to, color, piecePositions);
     case 'bishop':
-      return validBishopMove(from, to);
+      return validBishopMove(from, to, color, piecePositions);
     case 'rook':
-      return validRookMove(from, to);
+      return validRookMove(from, to, color, piecePositions);
     case 'queen':
-      return validQueenMove(from, to);
+      return validQueenMove(from, to, color, piecePositions);
     case 'king':
-      return validKingMove(from, to);
+      return validKingMove(from, to, color, piecePositions);
     case 'pawn':
       if (color === undefined) throw new Error('color is required for pawn moves');
-      return validPawnMove(from, to, color);
+      return validPawnMove(from, to, color, piecePositions);
     default:
       return false;
   }
 }
 
-function validKnightMove([fromX, fromY]: number[], [toX, toY]: number[]) {
+function validKnightMove(
+  [fromX, fromY]: number[],
+  [toX, toY]: number[],
+  color: PieceColor,
+  piecePositions: PiecePositions,
+) {
   const dx = Math.abs(toX - fromX);
   const dy = Math.abs(toY - fromY);
-
   // A knight moves in an L shape, so by 2 squares in one axis and 1 in the other.
-  return (dx === 2 && dy === 1) || (dx === 1 && dy === 2);
+  const squareIsValid = (dx === 2 && dy === 1) || (dx === 1 && dy === 2);
+
+  return squareIsValid && !squareContainsSameColorPiece(toX, toY, color, piecePositions);
 }
 
-function validBishopMove([fromX, fromY]: number[], [toX, toY]: number[]) {
+function validBishopMove(
+  [fromX, fromY]: number[],
+  [toX, toY]: number[],
+  color: PieceColor,
+  piecePositions: PiecePositions,
+) {
   const dx = Math.abs(toX - fromX);
   const dy = Math.abs(toY - fromY);
-
   // A bishop moves diagonally, so the absolute change in x and y must be equal,
   // i.e. if it moves one square up/down, it must also move one square left/right.
   // It must also move at least one square.
-  return dx === dy && dx > 0;
+  const squareIsValid = dx === dy && dx > 0 && dy > 0;
+
+  return squareIsValid && !squareContainsSameColorPiece(toX, toY, color, piecePositions);
 }
 
-function validRookMove([fromX, fromY]: number[], [toX, toY]: number[]) {
+function validRookMove(
+  [fromX, fromY]: number[],
+  [toX, toY]: number[],
+  color: PieceColor,
+  piecePositions: PiecePositions,
+) {
   const dx = Math.abs(toX - fromX);
   const dy = Math.abs(toY - fromY);
-
   // A rook moves horizontally or vertically, so can only move in one axis at a time.
   // That means one axis must stay the same. It must also move at least one square
   // along either axis.
-  return (dx === 0 || dy === 0) && (dx > 0 || dy > 0);
+  const squareIsValid = (dx === 0 || dy === 0) && (dx > 0 || dy > 0);
+
+  return squareIsValid && !squareContainsSameColorPiece(toX, toY, color, piecePositions);
 }
 
-function validQueenMove(from: number[], to: number[]) {
+function validQueenMove(
+  from: number[],
+  to: number[],
+  color: PieceColor,
+  piecePositions: PiecePositions,
+) {
   // A queen is effectively a combination of a rook and a bishop - it can move
   // horizontally, vertically, or diagonally.
-  return validRookMove(from, to) || validBishopMove(from, to);
+  // horizontally, vertically, or diagonally.
+  return (
+    validRookMove(from, to, color, piecePositions) ||
+    validBishopMove(from, to, color, piecePositions)
+  );
 }
 
-function validKingMove([fromX, fromY]: number[], [toX, toY]: number[]) {
+function validKingMove(
+  [fromX, fromY]: number[],
+  [toX, toY]: number[],
+  color: PieceColor,
+  piecePositions: PiecePositions,
+) {
   const dx = Math.abs(toX - fromX);
   const dy = Math.abs(toY - fromY);
-
   // A king can move in any direction, but only one square at a time.
-  return (dx > 0 || dy > 0) && dx <= 1 && dy <= 1;
+  const squareIsValid = (dx > 0 || dy > 0) && dx <= 1 && dy <= 1;
+
+  return squareIsValid && !squareContainsSameColorPiece(toX, toY, color, piecePositions);
 }
 
-function validPawnMove([fromX, fromY]: number[], [toX, toY]: number[], color: PieceColor) {
+function validPawnMove(
+  [fromX, fromY]: number[],
+  [toX, toY]: number[],
+  color: PieceColor,
+  piecePositions: PiecePositions,
+) {
   const dx = toX - fromX;
   const dy = toY - fromY;
 
   // A pawn can move forward one square
   // TODO implement capturing diagonally
   // TODO implement moving two squares if it hasn't moved yet
+  let squareIsValid;
   if (color === 'black') {
-    return dx === 0 && dy === 1;
+    squareIsValid = dx === 0 && dy === 1;
   } else {
-    return dx === 0 && dy === -1;
+    squareIsValid = dx === 0 && dy === -1;
   }
+
+  return squareIsValid && !squareContainsSameColorPiece(toX, toY, color, piecePositions);
+}
+
+function squareContainsSameColorPiece(
+  x: number,
+  y: number,
+  color: PieceColor,
+  piecePositions: PiecePositions,
+) {
+  const pieceOnSquare = piecePositions[`${x},${y}`];
+  return pieceOnSquare && pieceOnSquare.color === color;
 }
