d949544fbc5758ca75ab33782b2ac816fdb304f4
Ted Leahy <leahyted@proton.me>
2025-03-18 22:45:51 +0000

    Allow <Dialog> component to take a background color prop

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -3,21 +3,28 @@ import { PropsWithChildren } from 'react';
 type DialogProps = {
     open: boolean;
     header?: string;
+    backgroundColor?: string;
 };
 
-export default function Dialog({ open, header, children }: PropsWithChildren<DialogProps>) {
+export default function Dialog({
+    open,
+    header,
+    children,
+    backgroundColor = 'white',
+}: PropsWithChildren<DialogProps>) {
     return (
         <dialog
             style={{
-                zIndex: 1,
+                zIndex: 2,
                 alignSelf: 'center',
                 padding: '2rem 3rem',
                 textAlign: 'center',
                 width: '40rem',
+                backgroundColor: backgroundColor,
             }}
             open={open}
         >
-            {header && <h4 style={{ marginTop: 0 }}>{header}</h4>}
+            {header && <h3 style={{ marginTop: 0 }}>{header}</h3>}
             {children}
         </dialog>
     );
